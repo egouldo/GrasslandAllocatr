3bcb7c82703d5af31ef4679ab9892e552bf8f094
egouldo <elisebgould@gmail.com>
2016-08-23 11:10:47 +1000

    Add roxygen documentation for param/return df’s
    And also export %>% from magrittr to avoid function ‘%>%’ can’t be found error during remake::make() call.
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/summarise_by_transect.R
+++ R/summarise_by_transect.R
@@ -1,11 +1,11 @@
 #' Summarise by quadrat data into per transect data with key variables for updating the GrasslandBBN
 #'
-#' @param field_data_by_quadrat
-#'
-#' @return field_data_by_transect
+#' @param field_data_by_quadrat  A tidy dataframe grouped by transect number and quadrat with multiple entries for each quadrat under the variable \code{species}. The species column includes all species encountered within the quadrat, as well as non-living entities such as Bare Ground and Litter, and non-vascular life-forms including lichen and moss. Each \code{species} has a \code{type}, an \code{origin'}, and \code{growth-form} attribute, listed under the respective variable columns. Information about each transect is also included under the variables: \code{date} of survey, \code{orientation} of transect, survey \code{assistant}, last \code{management} action, \code{burn_season} of the last fire, \code{years_since} last biomass removal, and \code{biomass_reduction_year}
+#' @return field_data_by_transect A data_frame containing a row for each surveyed transect. Measured variables are equivalent to the nodes in the GrasslandBBN, and include: \code{E_pc}, the mean percent cover of exotic species per transect; \code{BG_pc} mean percent area of Bare Ground for each transect; \code{NF_diversity}, the total number of native forbs in each transect; \code{E_diversity}, the total number of exotic species across each transect.
 #' @export
 #' @import dplyr
 #' @import tidyr
+#' @importFrom magrittr %>%
 #'
 summarise_by_transect <- function(field_data_by_quadrat) {
         # Get percent_cover variables
@@ -29,10 +29,17 @@ summarise_by_transect <- function(field_data_by_quadrat) {
                 dplyr::filter(type == "E" | type == "NF") %>%
                 tidyr::spread(type, n) %>%
                 dplyr::rename(NF_diversity = NF, E_diversity = E)
-        # Get Management History variables
+        # Get Management History and Site Info variables
         management_history_dat <-
                 field_data_by_quadrat %>%
-                dplyr::select(transect_number, management, years_since) %>%
+                dplyr::select(transect_number,
+                              management,
+                              years_since,
+                              biomass_reduction_year,
+                              size,
+                              date,
+                              orientation,
+                              assistant) %>%
                 dplyr::distinct()
         # Merge all data and return:
         field_data_by_transect <- dplyr::left_join(percent_cover_dat, diversity_dat) %>%
