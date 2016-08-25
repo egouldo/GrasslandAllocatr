d0f0895c310e566804a6c5b80bb3822ea5e52ebc
egouldo <elisebgould@gmail.com>
2016-08-25 12:12:51 +1000

    Regenerate management lookup with +var mngmnt_unit
    This is to support functionality within the generate_case_file_data_frame() fn whereby multiple transects per management unit are aggregated. The ‘raw_data_2014’ file has already been aggregated for the management units with multiple transects. But adding this functionality to the function allows for other users and possibly the 2015 data to be aggregated where there are large management units with multiple transects.
    
    Also note that I have changed the numbering on the field_site_management lookup file to account for the method of aggregation in the generate_case_file_data_frame() fn (group by management_unit).
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/summarise_by_transect.R
+++ R/summarise_by_transect.R
@@ -1,7 +1,9 @@
 #' Summarise by quadrat data into per transect data with key variables for updating the GrasslandBBN
 #'
-#' @param field_data_by_quadrat  A tidy dataframe grouped by transect number and quadrat with multiple entries for each quadrat under the variable \code{species}. The species column includes all species encountered within the quadrat, as well as non-living entities such as Bare Ground and Litter, and non-vascular life-forms including lichen and moss. Each \code{species} has a \code{type}, an \code{origin'}, and \code{growth-form} attribute, listed under the respective variable columns. Information about each transect is also included under the variables: \code{date} of survey, \code{orientation} of transect, survey \code{assistant}, last \code{management} action, \code{burn_season} of the last fire, \code{years_since} last biomass removal, and \code{biomass_reduction_year}
+#'
 #' @return field_data_by_transect A data_frame containing a row for each surveyed transect. Measured variables are equivalent to the nodes in the GrasslandBBN, and include: \code{E_pc}, the mean percent cover of exotic species per transect; \code{BG_pc} mean percent area of Bare Ground for each transect; \code{NF_diversity}, the total number of native forbs in each transect; \code{E_diversity}, the total number of exotic species across each transect.
+#'
+#' @param field_data_by_quadrat A tidy dataframe grouped by transect number and quadrat with multiple entries for each quadrat under the variable \code{species}. The species column includes all species encountered within the quadrat, as well as non-living entities such as Bare Ground and Litter, and non-vascular life-forms including lichen and moss. Each \code{species} has a \code{type}, an \code{origin}, and \code{growth-form} attribute, listed under the respective variable columns. Information about each transect is also included under the variables: \code{date} of survey, \code{orientation} of transect, survey \code{assistant}, last \code{management} action, \code{burn_season} of the last fire, \code{years_since} last biomass removal, and \code{biomass_reduction_year}.
 #' @export
 #' @import dplyr
 #' @import tidyr
@@ -39,7 +41,8 @@ summarise_by_transect <- function(field_data_by_quadrat) {
                               size,
                               date,
                               orientation,
-                              assistant) %>%
+                              assistant,
+                              management_unit) %>%
                 dplyr::distinct()
         # Merge all data and return:
         field_data_by_transect <- dplyr::left_join(percent_cover_dat, diversity_dat) %>%
