21c7b8a60e9bc541bb82f7cf0a3a23cc9d856c45
egouldo <elisebgould@gmail.com>
2016-11-15 17:30:12 +1100

    fix #8 wrong node name for management
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/generate_case_file_data_frame.R
+++ R/generate_case_file_data_frame.R
@@ -9,6 +9,8 @@
 #' @import dplyr
 #' @importFrom magrittr %<>%
 #'
+#' @details Please ensure the following rows are supplied: \code{management_unit}, \code{BG_mean}, \code{E_mean}, \code{E_diversity}, \code{NF_diversity}, \code{management}, \code{years_since}. Note that if you are providing observations for Management period t0, the column \code{management} is still required. You can fill this character variable with \code{NA}, the function will remove it as necessary since there is no \code{t0} \code{management} period.
+#'
 generate_case_file_data_frame <- function(field_data_by_transect, year = c(0:5)) {
         # Aggregate all replicate transect sampling for each key measure var
         # over each management_unit
@@ -21,7 +23,7 @@ generate_case_file_data_frame <- function(field_data_by_transect, year = c(0:5))
                                  NF_diversity = mean(NF_diversity)) %>%
                 dplyr::mutate(Grassland_Condition = "*")
         # Rename variable names to node names
-        year <- ifelse(year == 0, "", as.character(year))
+        year_char <- ifelse(year == 0, "", as.character(year))
         field_data_by_management_unit %<>%
                 tidyr::gather(variable, value, -management_unit) %>%
                 dplyr::mutate(variable =
@@ -32,10 +34,10 @@ generate_case_file_data_frame <- function(field_data_by_transect, year = c(0:5))
                                                     ifelse(variable ==
                                                                    "E_diversity",
                                                            "WeedDiversity",
-                                                           ifelse(variable == "years_since",
-                                                                  "YearsSince", ifelse(
+                                                           ifelse(variable == "year_chars_since",
+                                                                  "year_charsSince", ifelse(
                                                                   variable == "NF_diversity", "IndigSpp_transect", "Grassland_Condition")))))) %>%
-                dplyr::mutate(time = paste0("t", year)) %>%
+                dplyr::mutate(time = paste0("t", year_char)) %>%
                 tidyr::unite(variable, variable, time) %>%
                 tidyr::spread(variable, value)
 
@@ -44,15 +46,20 @@ generate_case_file_data_frame <- function(field_data_by_transect, year = c(0:5))
         # and collapsing management units with multiple entries (transects)
         # into single entries (1 summarised value per management unit)
         field_data_by_management_unit %<>%
-                dplyr::left_join({field_data_by_transect %>%
+                dplyr::left_join({
+                        field_data_by_transect %>%
                         dplyr::select(management_unit,
                                       management,
                                       years_since) %>%
                                 dplyr::rename(Management = management, YearsSince = years_since) %>%
                                 tidyr::gather(variable, value, -management_unit) %>%
-                                dplyr::mutate(time = paste0("t", year)) %>%
+                                dplyr::mutate(year = ifelse(variable == "Management", year - 1, year), year = as.character(year),
+                                              year = ifelse(year == 0, "",year),
+                                        time = paste0("t", year)) %>% dplyr::select(-year) %>%
                                 tidyr::unite(variable, variable, time) %>%
+                                dplyr::filter(variable != "Management_t-1") %>%
                                 tidyr::spread(variable, value)})
+
 
         return(field_data_by_management_unit)
 }
