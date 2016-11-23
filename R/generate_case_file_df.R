09283a37e72d9286e5bc0a078a2121d1ce40b4ba
egouldo <elisebgould@gmail.com>
2016-11-23 13:34:43 +1030

    fix #9 filter out illegal states Management nodes
    see issue #9 for deets.
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/generate_case_file_df.R
+++ R/generate_case_file_df.R
@@ -4,11 +4,30 @@
 #'
 #' @return casefile_df A dataframe of the same dimensions as \code{field_data_by_mu}, but with the \code{management_unit} column removed and a new \code{IDnum} column.
 #' @export
-#' @import dplyr
+#' @import dplyr, magrittr
 #'
 generate_case_file_df <- function(field_data_by_mu){
+
         casefile_df <- field_data_by_mu %>%
-                tbl_df() %>%
+                tbl_df()
+
+        # Ensure that only management actions considered in the model are present within casefile:
+        candidate_actions <- c("No_Management",
+                               "WC",
+                               "Fire_WC",
+                               "Grazing_WC",
+                               "SowingForbs_WC")
+        management_df <-
+                casefile_df %>%
+                dplyr::select(management_unit, starts_with("Management"))
+        colnames(management_df) <- c("management_unit", "management")
+        management_df %<>%
+                dplyr::filter(management %in% candidate_actions)
+        casefile_df %<>%
+                dplyr::select(-starts_with("Management",ignore.case = FALSE)) %>%
+                dplyr::right_join(.,management_df, by = "management_unit")
+        # Remove management_unit col, add IDnum col:
+        casefile_df <-  casefile_df %>%
                 dplyr::select(-management_unit) %>%
                 dplyr::mutate(IDnum = c(1:n()))
         return(casefile_df)
