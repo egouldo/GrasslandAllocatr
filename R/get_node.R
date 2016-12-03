562811588fe6393f694afba56e3524c87c518179
egouldo <elisebgould@gmail.com>
2016-12-03 09:11:52 +0800

    rename arg avoid namespace conflict
    argument `match_string` is also a function from, I think, the stringr package. This was causing evaluation issues because downstream functions are expecting a character vector, rather than a function.
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/get_node.R
+++ R/get_node.R
@@ -1,20 +1,19 @@
 #' Extract active nodes using partial matches:
-#' @param match_string The character string to search.
+#' @param string_to_match The character string to search.
 #' @param network Character string of the active target network's name
 #' @param node_names Defaults to \code{FALSE} and returns the matched active Netica node. If \code{TRUE}, returns the names of the matched nodes. Note that matching is currently case sensitive.
 #' @return Returns a character vector of the node names that match the searched string if \code{node_names = TRUE}, else returns a list of active netica
 #'  nodes that match the searched string if \code{node_names = FALSE}.
-#' @details Note that because this function returns a list when \code{node_names = FALSE}, this function can handle \code{match_string} vectors of length greater than 1, such that if you supply a multi-element vector, a list will be returned.
+#' @details Note that because this function returns a list when \code{node_names = FALSE}, this function can handle \code{string_to_match} vectors of length greater than 1, such that if you supply a multi-element vector, a list will be returned. Note that this function no longer supports partial matches at the moment due to an error with the function returning multiple concatenated matches when a partial match is found for more than one node.
 #' @export
 #' @import stringr
-get_node <- function(match_string = character, node_names = FALSE, network = grassland_bbn) {
-        out <- vector(mode = "list", length = length(match_string))
-        names(out) <- match_string
-        if(length(match_string)>1){
+get_node <- function(string_to_match = character, node_names = FALSE, network = grassland_bbn) {
+        out <- vector(mode = "list", length = length(string_to_match))
+        names(out) <- string_to_match
+        if(length(string_to_match)>1){
 
-                for(i in 1:length(match_string)){
-                        pos <- which(stringr::str_detect(NetworkAllNodes(network),
-                                                         match_string[[i]]))
+                for(i in 1:length(string_to_match)){
+                        pos <- match(x = string_to_match[[i]], table = names(NetworkAllNodes(network)))
                         out[i] <- if(node_names == FALSE){
                                 RNetica::NetworkAllNodes(network)[pos]
                                 } else(
@@ -25,7 +24,7 @@ get_node <- function(match_string = character, node_names = FALSE, network = gra
                 return(out)
 
         } else{
-                pos <- which(stringr::str_detect(NetworkAllNodes(network), match_string))
+                pos <- which(stringr::str_detect(NetworkAllNodes(network), string_to_match))
                 if(node_names == FALSE){
                         return(RNetica::NetworkAllNodes(network)[pos])
                 } else(
