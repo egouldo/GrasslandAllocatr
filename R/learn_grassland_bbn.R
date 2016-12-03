58817959f631eb409fa40da889769a6d1383802d
egouldo <elisebgould@gmail.com>
2016-12-03 09:14:06 +0800

    ensure licensekey loaded else fails
    This is because the free version of Netica can only handle networks of a limited number of nodes, but the number of nodes in our network is larger than that. During the remake::make() call, the Netica environment link to R is lost between commands, and the function fails because the password is unloaded.
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- R/learn_grassland_bbn.R
+++ R/learn_grassland_bbn.R
@@ -1,28 +1,37 @@
 #' Learn \code{grassland_bbn} with EM Learning algorithm
-#' @param network An active \code{Netica BN} object, defaults to \code{grassland_bbn}
+#' @param path Filepath to a grassland_bbn Netica network
 #' @param casefile_path A character string containing the path to the Netica casefile
 #' @param first_iteration Logical, is this the first iteration of learning by casefile? defaults to \code{TRUE}.
+#' @return grassland_bbn The R object pointer to the NeticaAPI network
 #' @export
-#' @import magrittr, RNetica, beepr
 #' @details Note if \code{first_iteration} is set to \code{TRUE}, node experience for target nodes will be set to 1 for every state. Please see Netica or RNetica documentation for information on the EM Learning algorithm.
 #'
-learn_grassland_bbn <- function(network, casefile_path, first_iteration = TRUE) {
-        # Get the network object (assume already loaded and compiled)
-        grassland_bbn %>% RNetica::is.active() # return error to user if not
-        grassland_bbn %>% RNetica::is.NetworkCompiled() # compile it if not
+learn_grassland_bbn <- function(path, casefile_path, first_iteration = TRUE, save_path) {
+        RNetica::StopNetica()
+        load_Netica_LicenseKey()
+
+        grassland_bbn <<- load_compile_grassland_bbn(path)
+        ClearAllErrors()
+
 
         # Select nodes for learning (get them from the casefile)
         cases <- RNetica::read.CaseFile(casefile_path)
         target_nodes <- colnames(cases)
         # Remove IDnum and any 'Management' nodes:
-        target_nodes <- target_nodes[-1] %>% get_node() %>%
-                base::Filter(function(x) !any(grepl("Management", x)), .)
+        target_nodes <- target_nodes[-1] %>% get_node(string_to_match = ., node_names = FALSE, network = grassland_bbn) %>%
+                base::Filter(function(x) !any(grepl("Management", x)), .) %>%
+                base::Filter(function(x) !any(grepl("IDnum", x)), .)
 
         # Set Node Experience to 1 for each state of each node, before learning:
         if(first_iteration == TRUE){
                 sapply(target_nodes, function(x) RNetica::NodeExperience(x)<-1)
         }
-        RNetica::LearnCPTs(nodelist = target_nodes,caseStream = casefile_path,method = "EM")
+
+        casefile_stream <- CaseFileStream(casefile_path)
+        RNetica::LearnCPTs(nodelist = target_nodes,
+                           caseStream = casefile_stream,
+                           method = "EM")
+        CloseCaseStream(casefile_stream)
         RNetica::ClearAllErrors()
-        beepr::beep(2)
+        RNetica::WriteNetworks(nets = grassland_bbn,paths = save_path)
 }
