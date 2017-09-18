0ca543dc6515db398f3dedc3cca270bb819f4241
egouldo <elisebgould@gmail.com>
2017-09-18 20:30:10 +1000

    load tar.gz source not .tgz binary
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- install-RNetica.sh
+++ install-RNetica.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tgz
-tar -xvf 'RNetica_0.4-6.tgz'
+wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tar.gz
+tar -xvf 'RNetica_0.4-6.tar.gz'
 R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/src/Netica_API_504'
