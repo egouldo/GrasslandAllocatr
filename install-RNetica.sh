076791df7dc0c5cba47ba21878eebd870eb3e793
egouldo <elisebgould@gmail.com>
2017-09-18 19:04:57 +1000

    sudo required for RNetica install
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- install-RNetica.sh
+++ install-RNetica.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tgz
 tar -xvf 'RNetica_0.4-6.tgz'
-R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/src/Netica_API_504'
+sudo R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/src/Netica_API_504'
