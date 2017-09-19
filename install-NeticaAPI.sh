0509f09de1d639c28461a6935d6e94905938378b
egouldo <elisebgould@gmail.com>
2017-09-19 14:02:43 +1000

    (travis) add shell scripts for linux netica build
    installs RNetica from source and downloads the Netica API
    
    Signed-off-by: egouldo <elisebgould@gmail.com>

---
+++ install-NeticaAPI.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+wget https://www.norsys.com/downloads/NeticaAPI_Linux.zip
+unzip 'NeticaAPI_Linux.zip'
+pwd
---
+++ install-RNetica.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tar.gz
+tar -xvf 'RNetica_0.4-6.tar.gz'
+R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/Netica_API_504/'
+rm -r ./RNetica/
+rm -r ./Netica_API_504/
