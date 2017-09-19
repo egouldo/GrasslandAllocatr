74e287ecc4975dab33f4388c2d7b4074bf0f76e4
egouldo <elisebgould@gmail.com>
2017-09-19 14:14:56 +1000

    how bout don’t remove write protected files
    Signed-off-by: egouldo <elisebgould@gmail.com>

--- install-RNetica.sh
+++ install-RNetica.sh
@@ -4,4 +4,3 @@ wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tar.gz
 tar -xvf 'RNetica_0.4-6.tar.gz'
 R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/Netica_API_504/'
 rm -r ./RNetica/
-rm -r ./Netica_API_504/
